c557781056a25638a6fd3377a8e5a43bd4415c1b
zhangsan <2558447316@qq.com>
2019-09-27 21:50:20 +0800

    fir

---
+++ src/main/java/com/qf/mapper/UserMapper.java
@@ -0,0 +1,9 @@
+package com.qf.mapper;
+
+/**
+ * @author zyh
+ * @date 2019/9/25 0025 16:50
+ * 佛祖保佑
+ */
+public interface UserMapper {
+}
---
+++ src/main/java/com/qf/service/UserService.java
@@ -0,0 +1,9 @@
+package com.qf.service;
+
+/**
+ * @author zyh
+ * @date 2019/9/25 0025 16:53
+ * 佛祖保佑
+ */
+public class UserService {
+}
